f9e497c0a78d86ebd034a0f74903627a57b7d004
sergioneto12 <sergio.ght14@gmail.com>
2021-05-26 21:49:56 -0300

    search added

--- frontend/src/Objects/index.js
+++ frontend/src/Objects/index.js
@@ -1,39 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardWrapper, Forms } from '../Styles/styles';
 import axios from 'axios';
 
-export default class Objects extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            objects: []
-        }
-    }
+export default function Objects() {
+    const [objects, setObjects] = useState([]);
+    const [term, setTerm] = useState('');
 
-    componentDidMount() {
+    console.log(objects)
+
+    useEffect(() => {
         axios.get(`http://localhost:5000/Products`)
             .then(res => {
-                console.log(res);
-                this.setState({ objects: res.data })
+                setObjects(res.data)
+                console.log(res.data)
             })
-            .catch(err => {
-                console.log(err);
-                this.setState(err);
+            .catch(error => {
+                console.log(error);
             });
-    }
+    })
 
-    handleDelete = event => {
+    const handleDelete = event => {
         event.preventDefault();
         axios.delete(`http://localhost:5000/Products/:id`)
-            .then(function (response) {
-                console.log(response);
+            .then(function (res) {
+                console.log(res);
             })
             .catch(function (error) {
                 console.log(error);
             });
     };
 
-    handlePatch = event => {
+    const handlePatch = event => {
         event.preventDefault();
         axios.patch(`http://localhost:5000/Products/:id`)
             .then(res => {
@@ -46,40 +43,31 @@ export default class Objects extends React.Component {
             })
     };
 
-    // handleText = (e) => {
-    //     const valor = e.target.value;
-    // }
-
-    listFilter = (e, objects) => {
-        const valor = e.target.value
-        if (objects.tipo !== valor) {
-            objects.splice(objects.indexOf(!valor), 1);
-            this.setState(objects);
-            console.log(objects)
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Forms method='GET'>
-                    <input placeholder='&#x1F50E;&#xFE0E; Search' onChange={(e) => this.handleText}></input>
-                    <button type='submit' onClick={this.listFilter}></button>
-                </Forms>
-                <Card>
-                    {this.state.objects.map(object =>
-                        <CardWrapper key={object.id}>
-                            <h1>{object.nome}</h1>
-                            <img src={object.url} alt='img' />
-                            <h2>R$ {object.valor}</h2>
-                            <div>
-                                <button className='b1' onClick={this.handlePatch}><i className="far fa-edit"></i></button>
-                                <button className='b2' onClick={this.handleDelete}><i className="far fa-trash-alt"></i></button>
-                            </div>
-                        </CardWrapper>
-                    )}
-                </Card>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Forms method='GET'>
+                <input type='text' placeholder='&#x1F50E;&#xFE0E; Search' onChange={event => { setTerm(event.target.value) }}></input>
+                <button type='submit'>Pesquisar</button>
+            </Forms>
+            <Card>
+                {objects.filter(object => {
+                    if (term == "") {
+                        return object
+                    } else if (object.nome.toLowerCase().includes(term.toLocaleLowerCase())) {
+                        return object
+                    }
+                }).map((object) =>
+                    <CardWrapper key={object.id}>
+                        <h1>{object.nome}</h1>
+                        <img src={object.url} alt='img' />
+                        <h2>R$ {object.valor}</h2>
+                        <div>
+                            <button className='b1' onClick={handlePatch}><i className="far fa-edit"></i></button>
+                            <button className='b2' onClick={handleDelete}><i className="far fa-trash-alt"></i></button>
+                        </div>
+                    </CardWrapper>
+                )}
+            </Card>
+        </div>
+    )
 }
