e81776309e9a77689b8f90f855b65d92c3fab5a1
sergioneto12 <sergio.ght14@gmail.com>
2021-05-28 16:15:16 -0300

    patch function added

--- frontend/src/Objects/index.js
+++ frontend/src/Objects/index.js
@@ -5,10 +5,11 @@ import axios from 'axios';
 export default function Objects() {
     const [objects, setObjects] = useState([]);
     const [term, setTerm] = useState('');
+    // const [item, setItem] = useState([])
 
     useEffect(() => {
         axios.get(`https://storechallenge12.herokuapp.com/Products`)
-        //axios.get(`http://localhost:5000/Products`)
+            //axios.get(`http://localhost:5000/Products`)
             .then(res => {
                 setObjects(res.data)
             })
@@ -18,35 +19,89 @@ export default function Objects() {
     }, []);
 
     async function handleDelete(object) {
-        console.log(object)
         axios.delete(`https://storechallenge12.herokuapp.com/Products/` + object)
-        //await axios.delete(`http://localhost:5000/Products/:id`)
+            //await axios.delete(`http://localhost:5000/Products/:id`)
             .then(function (res) {
                 console.log("Removido com sucesso!");
+                alert('Removido com sucesso');
             })
             .catch(function (error) {
                 console.log(error);
             });
     };
 
-    async function handlePatch() {
-        //axios.patch(`https://storechallenge12.herokuapp.com/Products/:id`)
-        await axios.patch(`http://localhost:5000/Products/:id`)
-            .then(res => {
-                console.log(res);
-                this.setState({ objects: res.data });
+    async function handlePatch(object) {
+        let resposta = window.prompt('O que você deseja modificar (nome, valor, tipo ou url da foto?');
+        console.log(resposta);
+
+        if (resposta == 'nome') {
+            await axios.patch(`https://storechallenge12.herokuapp.com/Products/` + object, {
+                nome: window.prompt('Qual é o novo nome, então? '),
             })
-            .catch(err => {
-                console.log(err);
-                this.setState(err);
+                .then(res => {
+                    console.log(object);
+                    alert('Atualizado com sucesso');
+                    document.location.reload(true)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } 
+
+        if (resposta == 'tipo') {
+            await axios.patch(`https://storechallenge12.herokuapp.com/Products/` + object, {
+                tipo: window.prompt('Qual é o novo tipo, então? '),
             })
+                .then(res => {
+                    console.log(object);
+                    alert('Atualizado com sucesso');
+                    document.location.reload(true)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } 
+
+        if (resposta == 'valor') {
+            await axios.patch(`https://storechallenge12.herokuapp.com/Products/` + object, {
+                valor: window.prompt('Qual é o novo valor, então? '),
+            })
+                .then(res => {
+                    console.log(object);
+                    alert('Atualizado com sucesso');
+                    document.location.reload(true)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } 
+
+        if (resposta == 'url') {
+            await axios.patch(`https://storechallenge12.herokuapp.com/Products/` + object, {
+                url: window.prompt('Qual é a nova url, então? '),
+            })
+                .then(res => {
+                    console.log(object);
+                    alert('Atualizado com sucesso');
+                    document.location.reload(true)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } 
     };
 
     return (
         <div>
             <Forms method='GET'>
-                <input type='text' placeholder='&#x1F50E;&#xFE0E; Search' onChange={event => { setTerm(event.target.value) }}></input>
-                <button type='submit'>Pesquisar</button>
+                <input
+                    type='text'
+                    placeholder='&#x1F50E;&#xFE0E; Search'
+                    onChange={
+                        event => { setTerm(event.target.value) }
+                    }>
+                </input>
+                {/* <button type='submit'>Pesquisar</button> */}
             </Forms>
             <Card>
                 {objects.filter(object => {
@@ -61,8 +116,25 @@ export default function Objects() {
                         <img src={object.url} alt='img' />
                         <h2>R$ {object.valor}</h2>
                         <div>
-                            <button className='b1' onClick={handlePatch}><i className="far fa-edit"></i></button>
-                            <button className='b2' onClick={() => handleDelete(object.id)}><i className="far fa-trash-alt"></i></button>
+
+                            <button
+                                className='b1'
+                                onClick={
+                                    () => handlePatch(object.id)
+                                }
+                            >
+                                <i className="far fa-edit"></i>
+                            </button>
+
+                            <button
+                                className='b2'
+                                onClick={
+                                    () => handleDelete(object.id)
+                                }
+                            >
+                                <i className="far fa-trash-alt"></i>
+                            </button>
+
                         </div>
                     </CardWrapper>
                 )}
